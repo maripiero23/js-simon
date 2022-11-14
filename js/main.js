1b4084782e16d77fbf47255c7542c163f35d263b
maripiero23 <mari.pieropan@gmail.com>
2022-11-14 16:19:30 +0100

    settimeout

--- js/main.js
+++ js/main.js
@@ -17,7 +17,19 @@ while( numeriRandom.length < 5){
 
     numeriRandom.push(num);
     console.log(numeriRandom);
+
+
+
+    
 }
 
 const casualeEl = document.querySelector(".casuale");
 casualeEl.innerHTML = pippo;
+
+
+setTimeout(displayVanish, 5000);
+
+function displayVanish(){
+
+    casualeEl.classList.add("d-none");
+}
